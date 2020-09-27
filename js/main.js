a792c4bf2508513017ce6ae4524e44bba868cc97
Karina <karina_karapetyan@mail.ru>
2020-09-27 16:43:45 +0400

    Задание 2 по личному проекту

--- js/main.js
+++ js/main.js
@@ -12,11 +12,12 @@ const MAP_START_Y = 130;
 const MAP_END_Y = 630;
 const map = document.querySelector(`.map`);
 const mapPins = map.querySelector(`.map__pins`);
+const mapFilterContainer = document.querySelector(`.map__filters-container`);
 const mapPinTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
+const cardPopupTemplate = document.querySelector(`#card`).content.querySelector(`.popup`);
 const fragment = document.createDocumentFragment();
 const similarAdverts = [];
 const advertAvatars = [];
-
 
 let coordX;
 let coordY;
@@ -41,7 +42,7 @@ const getUniqueNumbersInRange = function (min, max, num) {
 };
 
 const getRandomElementOfArray = function (array) {
-  return array[getRandomInRange(0, array.length)];
+  return array[Math.floor(Math.random() * array.length)];
 };
 
 const createUniqueAvatarArray = function (array, arrayLength) {
@@ -110,4 +111,66 @@ createAdvertsArray(similarAdverts, ADVERT_TYPES, ADVERT_CHECK_TIMES, ADVERT_FEAT
 for (let i = 0; i < similarAdverts.length; i++) {
   fragment.appendChild(renderAdvert(similarAdverts[i]));
 }
+
 mapPins.appendChild(fragment);
+
+// Не получается передать method в параметр функции checkDataExists (например, чтобы можно было заменить метод innerHTML на другой)
+const checkDataExists = function (element, data, elementSelector, value) {
+  // Expected an assignment or function call and instead saw an expression - эта ошибка возникает, потому что функция должна что-то возравщать? Не совсем понимаю, что можно вовзращать в этой функции.
+  data ? element.querySelector(elementSelector).innerHTML = value : element.querySelector(elementSelector).style.display = `none`;
+};
+
+const renderCard = function (advert) {
+  let cardPopupElement = cardPopupTemplate.cloneNode(true);
+
+  checkDataExists(cardPopupElement, advert.offer.title, `.popup__title`, advert.offer.title);
+  checkDataExists(cardPopupElement, advert.offer.address, `.popup__text--address`, advert.offer.address);
+  checkDataExists(cardPopupElement, advert.offer.price, `.popup__text--price`, `${advert.offer.price}₽/ночь`);
+  // Стоит, наверное, разделить проверки advert.offer.rooms && advert.offer.guests, advert.offer.checkin && advert.offer.checkout и предложение выводить по частям или так можно оставить?
+  checkDataExists(cardPopupElement, advert.offer.rooms && advert.offer.guests, `.popup__text--capacity`, `${advert.offer.rooms} комнаты для ${advert.offer.guests} гостей`);
+  checkDataExists(cardPopupElement, advert.offer.checkin && advert.offer.checkout, `.popup__text--time`, `Заезд после ${advert.offer.checkin}, выезд&nbsp;до ${advert.offer.checkout}`);
+  checkDataExists(cardPopupElement, advert.offer.features, `.popup__features`, advert.offer.features);
+  checkDataExists(cardPopupElement, advert.offer.description, `.popup__description`, advert.offer.description);
+
+  // Не получается передать method в параметр функции checkDataExists, чтобы можно было использовать эту функцию и для свойства src аватара, а не только innerHTML.
+  // Пока что advert.author.avatar оставила без проверки, прописать проверку без функции? Или можно реализовать передачу метода в аргумент?
+
+  cardPopupElement.querySelector(`.popup__avatar`).src = advert.author.avatar;
+
+  let elementType;
+
+  switch (advert.offer.type) {
+    case `palace`:
+      elementType = `Дворец`;
+      break;
+    case `flat`:
+      elementType = `Квартира`;
+      break;
+    case `house`:
+      elementType = `Дом`;
+      break;
+    case `bungalow`:
+      elementType = `Бунгало`;
+      break;
+    default:
+      elementType = ``;
+  }
+
+  // Если типа жилья нет, оно все-равно вставляет значение 'квартира', хоть и скрывает .popup__type - хотя я прописала в default пустую строку.
+
+  checkDataExists(cardPopupElement, advert.offer.type, `.popup__type`, elementType);
+
+  let elementPhotos = cardPopupElement.querySelector(`.popup__photos`);
+
+  if (advert.offer.photos) {
+    for (let i = 0; i < advert.offer.photos.length; i++) {
+      elementPhotos.insertAdjacentHTML(`beforeend`, `<img src="${advert.offer.photos[i]}" class="popup__photo" width="45" height="40" alt="Фотография жилья">`);
+    }
+  } else {
+    elementPhotos.style.display = `none`;
+  }
+
+  return cardPopupElement;
+};
+
+map.insertBefore(renderCard(similarAdverts[0]), mapFilterContainer);
